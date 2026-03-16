3f477b74a8a97057808b6694bc2410ba326cb9e2
taikhoantemp9999 <58102912+taikhoantemp9999@users.noreply.github.com>
2026-03-16 13:02:32 +0700

    tính năng offlone cho sơ đồ

---
+++ sw.js
@@ -0,0 +1,32 @@
+﻿const CACHE_NAME = 'ksattt-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/building.html',
+    '/style.css',
+    '/building.css',
+    '/script.js',
+    '/building.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
